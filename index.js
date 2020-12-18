eda71ccb77a43f2539470413a46dd9901722d42c
FAYEMI BOLUWATIFE <67077455+BOVAGE@users.noreply.github.com>
2020-12-17 17:18:09 -1200

    roll button

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-// gets the img elements
-let playerOneDice = document.querySelector(".img1");
-let playerTwoDice = document.querySelector(".img2");
+function diceGame () {
+	// gets the img elements
+	let playerOneDice = document.querySelector(".img1");
+	let playerTwoDice = document.querySelector(".img2");
 
-let result = document.querySelector("h1");
+	let result = document.querySelector("h1");
 
-// generates random dice number (1-6
-diceNoOne = Math.floor((Math.random() * 6) + 1);
-diceNoTwo = Math.floor((Math.random() * 6) + 1);
+	// generates random dice number (1-6
+	diceNoOne = Math.floor((Math.random() * 6) + 1);
+	diceNoTwo = Math.floor((Math.random() * 6) + 1);
 
-// gets the dice image
-let diceOne = "images/dice"+ diceNoOne +".png";
-let diceTwo = "images/dice"+ diceNoTwo +".png";
+	// gets the dice image
+	let diceOne = "images/dice"+ diceNoOne +".png";
+	let diceTwo = "images/dice"+ diceNoTwo +".png";
 
-// sets the src to the dice image
-playerOneDice.setAttribute("src", diceOne);
-playerTwoDice.setAttribute("src", diceTwo);
+	// sets the src to the dice image
+	playerOneDice.setAttribute("src", diceOne);
+	playerTwoDice.setAttribute("src", diceTwo);
 
 
-if (diceNoOne > diceNoTwo) {
-	result.textContent = "Player 1 wins!";
-} else if (diceNoOne === diceNoTwo) {
-	result.textContent = "DRAW!";
-} else {
-	result.textContent = "Player 2 wins!";
+	if (diceNoOne > diceNoTwo) {
+		result.textContent = "Player 1 wins!";
+	} else if (diceNoOne === diceNoTwo) {
+		result.textContent = "DRAW!";
+	} else {
+		result.textContent = "Player 2 wins!";
+	}
 }
 
 
+document.getElementById("roll").addEventListener("click", diceGame);
